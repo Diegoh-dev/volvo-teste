0f8b59e01f23bdab74e06d501181c33c72001543
Diegoh-dev <diegoh.developer@gmail.com>
2023-06-29 22:58:27 -0300

    feat[submenu]: filtros por tipo de carro.

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,7 +1,9 @@
+import { SubMenu } from "@/components/Submenu/submenu";
+
 export default function Cars(){
   return (
     <div>
-      <h1>meus carros</h1>
+     <SubMenu/>
     </div>
   )
 }
--- src/components/Submenu/submenu.tsx
+++ src/components/Submenu/submenu.tsx
@@ -1,7 +1,16 @@
-export function SubMenu(){
-  return(
-    <section>
-      
+import styles from "./submenu.module.scss";
+
+export function SubMenu() {
+  return (
+    <section className={styles.containerSubmenu}>
+      {/* colocar um filtro em algum lugar */}
+      <h1>Todos os modelos Recharge</h1>
+
+      <div className={styles.filterButtons}>
+        <button className={styles.button}>Todos (6)</button>
+        <button className={styles.button}>SUV (5)</button>
+        <button className={styles.button}>Crossover (1)</button>
+      </div>
     </section>
-  )
+  );
 }
