e6e9fd115e43e2e72a536760d4734877eb328bef
Diegoh-dev <diegoh.developer@gmail.com>
2023-07-01 12:33:43 -0300

    feat[info]: componente de pequenas informações dos carros.

--- src/app/cars/page.tsx
+++ src/app/cars/page.tsx
@@ -1,9 +1,26 @@
+'use client'
 import { SubMenu } from "@/components/Submenu/submenu";
-
+import url from '../../../public/api/cars.json';
+import { useEffect, useState } from "react";
+import {Carousel} from '../../components/Carousel/carousel';
 export default function Cars(){
+
+//   const[listCars,setListCars] = useState([]);
+
+
+//  async function getDate(){
+//     const response = await fetch('api/cars.json');
+//     const data = await response.json();
+//     setListCars(data);
+//   }
+
+//   useEffect(() => {
+//     getDate();
+//   },[])
   return (
     <div>
      <SubMenu/>
+     <Carousel/>
     </div>
   )
 }
