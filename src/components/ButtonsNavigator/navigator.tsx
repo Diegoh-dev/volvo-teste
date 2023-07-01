347057e35351caec3869def285c3b0d3c7f9a549
Diegoh-dev <diegoh.developer@gmail.com>
2023-07-01 13:44:03 -0300

    feat[Button navigator]: passando as funções do scroll para o componente reponsavel.

--- src/components/ButtonsNavigator/navigator.tsx
+++ src/components/ButtonsNavigator/navigator.tsx
@@ -1,12 +1,20 @@
 import styles from './buttonNavigator.module.scss';
 
-export function ButtonsNavigator() {
+interface ButtonsNavigatorProps{
+  carousel:any;
+}
+
+export function ButtonsNavigator({carousel}:ButtonsNavigatorProps) {
 
   function handleLeftClick(e:any){
-    
+    e.preventDefault();
+    console.log(carousel.current.offsetWidth)
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
   }
   function handleRightClick(e:any){
-    
+    e.preventDefault();
+    console.log(carousel.current.offsetWidth)
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
   }
   return (
     <div className={styles.buttons}>
--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -2,13 +2,15 @@
 import { useEffect, useState,useRef } from 'react';
 import styles from './carousel.module.scss';
 import { Info } from '../Info/info';
+import { Links } from '../Links/links';
+import { ButtonsNavigator } from '../ButtonsNavigator/navigator';
 
 
 export function Carousel(){
 
   const[listCars,setListCars] = useState([]);
-
   const carousel  = useRef(null);
+
   async function getDate(){
      const response = await fetch('api/cars.json');
      const data = await response.json();
@@ -43,25 +45,21 @@ export function Carousel(){
         
         return(
             <div key={id} className={styles.item}>
-              <Info bodyType={bodyType} modelName={modelType}/>
+              <Info bodyType={bodyType} modelName={modelName}/>
             <div className={styles.image}>
             <img src={imageUrl} alt="carros" />
             </div>
-    
-            <div className={styles.info}>
-                <span className={styles.name}>carro 1</span>
-                <span className={styles.oldPrice}>51555</span>
-                <span className={styles.price}>6566656</span>
-            </div>
+            <Links/>
           </div>
          )
       })}
       </div>
 
-      <div className={styles.buttons}>
+      {/* <div className={styles.buttons}>
         <button onClick={handleLeftClick}>{'<'} </button>
         <button onClick={handleRightClick}>{'>'}</button>
-      </div>
+      </div> */}
+      <ButtonsNavigator carousel={carousel}/>
     </div>
   )
 }
