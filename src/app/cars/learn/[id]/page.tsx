02bb4dc0e31bfb77c11ccdeceaa28401036421d1
Diegoh-dev <diegoh.developer@gmail.com>
2023-07-01 21:09:44 -0300

    feat[learn]: iniciando header da pagina.

--- src/app/cars/learn/[id]/page.tsx
+++ src/app/cars/learn/[id]/page.tsx
@@ -1,15 +1,39 @@
+import styles from './learn.module.scss';
 
-interface LearnProps{
-  params:{
-    id:string;
-  }
+interface LearnProps {
+  params: {
+    id: string;
+  };
 }
 
-export default function Learn({params}:LearnProps){
-  return(
-
-    <div>
-      Next 13 {params.id}
+export default function Learn({ params }: LearnProps) {
+  const { id } = params;
+  return (
+    <div className={styles.container}>
+      {/* Next 13 {id} */}
+      <header className={styles.header}>
+        <nav>
+          <ul className={styles.list}>
+            <li>
+              <strong>{id}</strong>
+            </li>
+            <li>
+              Descrição geral
+            </li>
+            <li>
+              Interior
+            </li>
+            <li>
+              Características
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <button>
+            MANTENHA-ME INFORMADO
+          </button>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
