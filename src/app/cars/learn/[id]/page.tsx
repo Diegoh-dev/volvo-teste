5991893a50be2aaf7327852b22941d18ee60842d
Diegoh-dev <diegoh.developer@gmail.com>
2023-07-02 13:37:08 -0300

    style[learn]: finalizando informações do carro.

--- src/app/cars/learn/[id]/page.tsx
+++ src/app/cars/learn/[id]/page.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image';
-import styles from './learn.module.scss';
+import Image from "next/image";
+import styles from "./learn.module.scss";
 
 interface LearnProps {
   params: {
     id: string;
   };
-  imageUrl:string;
+  imageUrl: string;
 }
 
-export default function Learn({ params ,imageUrl}: LearnProps) {
+export default function Learn({ params, imageUrl }: LearnProps) {
   const { id } = params;
-  console.log('imageUrl',imageUrl)
+  console.log("imageUrl", imageUrl);
   return (
     <div className={styles.container}>
       <header className={styles.header}>
@@ -45,49 +45,91 @@ export default function Learn({ params ,imageUrl}: LearnProps) {
         />
       </section>
 
-      <section>
-        <div>
+      <section style={{
+        width:'100%',
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'center',
+        flexDirection:'column',
+        gap:'2rem',
+        }}>
+        <div style={{
+          marginTop:'1.5rem',
+          display:'flex',
+          alignItems:'center',
+          justifyContent:'center',
+          flexDirection:'column',
+          gap:'.5rem',
+        }}>
+          <h3 style={{
+            textTransform:'uppercase',
+          }}>
+          <strong>{id}</strong>.
+          </h3>
           <h2>
-            {" "}
-            <strong>{id}</strong>. Excelente desempenho <br /> elétrico num
+             Excelente desempenho elétrico num
             formato compacto.
           </h2>
         </div>
 
-        <article>
-          <div>
-            <h4>480km</h4>
-            <p>
+        <article style={{
+          display:'flex',
+          width:'100%',
+          alignItems:'center',
+          justifyContent:'space-around',
+          // gap:'2rem',
+          flexWrap:'wrap',
+          marginBottom:'2rem',
+        }}>
+          <div style={{
+            maxWidth:'150px',
+
+          }}>
+            <h4  style={{
+              fontSize:'40px',
+            }}>480km</h4>
+            <p style={{
+              color:'rgba(0, 0, 0, 0.64)'
+            }}>
               Autonomia elétrica <br />
               (Até)
             </p>
           </div>
 
-          <div>
-            <h4>Cerca de 30min</h4>
-            <p>
-              
-Estimativa de tempo do carregamento rápido DC
-
-(10%-80%)
-            </p>
+          <div style={{
+            maxWidth:'180px',
+            marginTop:'1rem',
+            
+          }}>
+            <h4  style={{
+              fontSize:'40px',
+              whiteSpace:'nowrap',
+            }}>Cerca de 30min</h4>
+            <p style={{
+              color:'rgba(0, 0, 0, 0.64)'
+            }}>Estimativa de tempo do carregamento rápido  DC (10%-80%)</p>
           </div>
-          <div>
-            <h4>15.7kWh</h4>
-            <p>
-              
-Consumo de energia
-
-(por 100km)
-            </p>
+          <div style={{
+            maxWidth:'150px',
+            
+          }}>
+            <h4  style={{
+              fontSize:'40px',
+            }}>15.7kWh</h4>
+            <p style={{
+              color:'rgba(0, 0, 0, 0.64)'
+            }}>Consumo de energia (por 100km)</p>
           </div>
-          <div>
-            <h4>5.3s</h4>
-            <p>
-            Aceleração
-
-(0-100 km/h)
-            </p>
+          <div style={{
+            maxWidth:'150px',
+            
+          }}>
+            <h4  style={{
+              fontSize:'40px',
+            }}>5.3s</h4>
+            <p style={{
+              color:'rgba(0, 0, 0, 0.64)'
+            }}>Aceleração (0-100 km/h)</p>
           </div>
         </article>
       </section>
